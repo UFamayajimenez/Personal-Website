b0e426bba56f886bab42b6f9e1145da4b6ac90fc
Amaya Jimenez <amaya.jimenez@ufl.edu>
2020-08-16 22:35:43 -0400

    added router with hello world home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link} from "react-router-dom";
+import Home from "./views/Home/Home";
 
 function App() {
   return (
+    <Router>
     <div>
-      <h1>hello world</h1>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
-  );
+  </Router>
+);
 }
 
 export default App;
