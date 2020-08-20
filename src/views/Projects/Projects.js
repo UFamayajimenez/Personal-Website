92afa06f1ce5fcad15ca66ee147c11675266a29e
Amaya Jimenez <amaya.jimenez@ufl.edu>
2020-08-20 17:34:27 -0400

    deleted containters and simplified structure

--- src/views/Projects/Projects.js
+++ src/views/Projects/Projects.js
@@ -5,13 +5,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Projects = () => {
     return (
         <div id="projects">
-           <div id="right-container">
-               <p>text</p>
-           </div>
-           <div id="left-container">
-               <p>text</p>
-           </div>
+            <h1>Projects</h1>
+           <div id="content">
+               <p>Here you can find the projects I am most proud of. 
+            They are categorized as either personal or educational projects. Educational projects are those completed while attending the University of Florida.</p>
+
+            <div id="project-content">
+                <h2>Personal</h2>
+
+                <h3>This Website ✨</h3>
+                    <h3>Wheel Map ♿</h3>
+                <h3>Kanye Slackbot 🎤</h3>
+                <h2>Educational</h2>
+                <h3>Heavenly Writing Horoscopes 🔮</h3>
+            </div>
         </div>
+    </div>
     );
 }
 
